131002009b9a6697efcc6c4c75f787734ec7c3a0
LucyWeltner <lweltner@bu.edu>
2020-06-05 12:07:11 -0400

    When a user selects a bird from the dropdown menu, a graph is displayed showing when and how often thatspecies was sighted.

--- birdwatching_app/src/App.js
+++ birdwatching_app/src/App.js
@@ -3,27 +3,65 @@ import './App.css';
 import Graph from './Components/graph.js'
 import SearchMenu from './Components/search_menu.js'
 
-function App() {
-  let birdData = require("./BirdData/bird_data.json")
-  birdData = JSON.stringify(birdData)
-  birdData = JSON.parse(birdData)
-  birdData = birdData.info.map(date => date.birds)
-  let allSightings = birdData.map(sightings => Object.keys(sightings))
-  allSightings = allSightings.flat()
-  let birdSpecies = {}
-  allSightings.forEach(species => {
-    if (!birdSpecies[species]) {
-        birdSpecies[species] = "yes"
-    }
-  })
-  console.log(birdSpecies)
-  return (
-    <div className="App">
-      <h2>Search For a Bird</h2>
-      <SearchMenu options={Object.keys(birdSpecies)}/>
-      <Graph title="stuff" data={[10, 20, 50, 5]}/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    birdSpecies: {},
+    data: [],
+    title: "stuff"
+  }
+  processData() {
+    let birdData = require("./BirdData/bird_data.json")
+    birdData = JSON.stringify(birdData)
+    birdData = JSON.parse(birdData)
+    birdData = birdData.info.map(date => date.birds)
+    let allSightings = birdData.map(sightings => Object.keys(sightings))
+    allSightings = allSightings.flat()
+    let birdSpecies = {}
+    allSightings.forEach(species => {
+      if (!birdSpecies[species]) {
+          let sightingsForSpecies = birdData.map(BirdsOnDate => {
+            if (BirdsOnDate[species]) {
+              return BirdsOnDate[species]
+            }
+            else {
+              return 0
+            }
+          })
+        birdSpecies[species] = sightingsForSpecies
+      }
+    })
+    this.setState({birdSpecies: birdSpecies}, () => console.log(this.state))
+  }
+
+  componentDidMount() {
+    this.processData()
+  }
+
+  makeGraph = (bird) => {
+    console.log("you selected " + bird)
+    console.log(this.state.birdSpecies[bird])
+    this.setState(prevState => {
+      return {...prevState, title: bird, data: prevState.birdSpecies[bird]}
+    })
+    // let graph = document.getElementById("graph")
+    // graphElement = <Graph title={bird} data={this.state.birdSpecies[bird]}/>
+    // ReactDOM.render(graphElement, graph)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Search For a Bird</h2>
+        <SearchMenu options={Object.keys(this.state.birdSpecies)} makeGraph={this.makeGraph}/>
+        <div id="graph">
+          <Graph title={this.state.title} data={this.state.data}/>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+
