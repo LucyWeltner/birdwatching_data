01a04caa63c956607207c477ff597e8194ff69d1
LucyWeltner <lucyiweltner@gmail.com>
2020-06-07 20:57:34 -0400

    When user mouses over a bar on a graph, the graph displays the number of sightings represented by that bar.

--- birdwatching_app/src/Components/bar.js
+++ birdwatching_app/src/Components/bar.js
@@ -1,8 +1,20 @@
 import React from "react"
 export default class Bar extends React.Component {
+	state = {
+		displaySightings: false
+	}
+
+	showHeight = () => {
+		this.setState({displaySightings: true})
+	}
+
 	render() {
+		let textbox = <text transform='rotate(90)' y="-2.5" font-size="5px" fill="white">{this.props.height/5} Sightings</text>
 		return (
-			<rect x={this.props.xAxis*10+15} y={180-this.props.height} height={this.props.height} width="10"/>
+			<g transform={`translate(${this.props.xAxis*10+15}, ${180-this.props.height})`}>
+				<rect onMouseOver={this.showHeight} height={this.props.height} width="10"/>
+				{this.state.displaySightings ? textbox : null}
+			</g> 
 		)
 	}
 }
