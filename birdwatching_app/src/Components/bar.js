f109b9ac5c625e8eef73a43e90dc514f7d029acc
LucyWeltner <lucyiweltner@gmail.com>
2020-06-07 21:22:41 -0400

    When bar is small, text is placed above bar; when bar is tall, text is placed inside bar. Bars now have grey borders.

--- birdwatching_app/src/Components/bar.js
+++ birdwatching_app/src/Components/bar.js
@@ -1,4 +1,6 @@
 import React from "react"
+import BarCSS from './bar.css'
+
 export default class Bar extends React.Component {
 	state = {
 		displaySightings: false
@@ -8,12 +10,24 @@ export default class Bar extends React.Component {
 		this.setState({displaySightings: true})
 	}
 
+	hideHeight = () => {
+		this.setState({displaySightings: false})
+	}
+
 	render() {
-		let textbox = <text transform='rotate(90)' y="-2.5" font-size="5px" fill="white">{this.props.height/5} Sightings</text>
+		let textbox = null
+		if (this.state.displaySightings) {
+			if (this.props.height > 20) {
+				textbox = <text transform='rotate(90)' y="-2.5" x="2.5" font-size="5px" fill="white">{this.props.height/5} Sightings</text>
+			}
+			else {
+				 textbox = <text font-size="5px" transform='rotate(90)' y="-2.5" x={-25} fill="black">{this.props.height/5} Sightings</text>
+			}
+		}
 		return (
-			<g transform={`translate(${this.props.xAxis*10+15}, ${180-this.props.height})`}>
-				<rect onMouseOver={this.showHeight} height={this.props.height} width="10"/>
-				{this.state.displaySightings ? textbox : null}
+			<g class="bar" transform={`translate(${this.props.xAxis*10+15}, ${180-this.props.height})`}>
+				<rect onMouseOver={this.showHeight} onMouseLeave={this.hideHeight} height={this.props.height} width="10" stroke="grey" stroke-width="1"/>
+				{textbox}
 			</g> 
 		)
 	}
