2a7f39069c69b9c884c2a8376afeb29c6f7b4247
LucyWeltner <lucyiweltner@gmail.com>
2020-06-23 16:15:35 -0400

    Expanded JSON data to include links to images of each bird species seen

--- birdwatching_app/src/Components/search_menu.js
+++ birdwatching_app/src/Components/search_menu.js
@@ -2,13 +2,13 @@ import React from "react"
 
 export default class SeachMenu extends React.Component {
 	state = {
-		selectedBird: "all_birds"
+		selected: "all_birds"
 	}
 	render() {
 		let sortedOptions = this.props.options.sort()
 			return(
-				<form onSubmit={event => {event.preventDefault(); this.props.makeGraph(this.state.selectedBird)}}>
-					<select name="birds" onChange={event => this.setState({selectedBird: event.target.value})}>
+				<form onSubmit={event => {event.preventDefault(); this.props.makeGraph(this.state.selected)}}>
+					<select name="birds" onChange={event => this.setState({selected: event.target.value})}>
 						{sortedOptions.map((species, index) => <option value={species} key={index}>{species.replace(/_/g, " ")}</option>)}
 					</select>
 					<input type="submit"/>
