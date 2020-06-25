57b2408676fcc3eff57da8f1cc8aae4ed890222b
LucyWeltner <lucyiweltner@gmail.com>
2020-06-25 17:18:08 -0400

    a dds CSS styling to body, submit buttons and text in graphs

--- birdwatching_app/src/Components/graph.js
+++ birdwatching_app/src/Components/graph.js
@@ -6,23 +6,25 @@ import graphCSS from './graph.css'
 export default class Graph extends React.Component {
 	render() {
 		let scale 
-		if (_.max(this.props.data) > 50) {
+		if (_.max(this.props.data) >= 50) {
 			scale = 1
 		}
-		else if (_.max(this.props.data) > 20) {
+		else if (_.max(this.props.data) >= 40) {
+			scale = 1.5
+		}
+		else if (_.max(this.props.data) >= 18) {
 			scale = 2
 		}
 		else {	
 			scale = 3
 		}
 		return (
-				<svg viewBox = "0 0 200 300">
+				<svg viewBox = {`0 0 200 ${250/scale}`}>
 					<text id="title" x="10" y ="20">{this.props.title}</text>
-					<br />
 					<line x1="10" y1="25" x2="10" y2={180/scale} stroke="grey"/>
 					<line x1="10" y1={180/scale} x2="180" y2={180/scale} stroke="grey"/>
 					{this.props.data.map((height, index) => <Bar key={index} scale={scale} xAxis={index} height={height*scale} scale={scale} info={this.props.info}/>)}
-					{this.props.dates.map((date, index) => <text x={index*10+15} fontSize="4" y={180/scale + 6} transform={`rotate(90 ${index*10+15}, ${180/scale + 5})`} key={index} info={this.props.info}>{date.replace(/_/g, " ") +" "}</text>)}
+					{this.props.dates.map((date, index) => <text x={index*10+13} fontSize="4" y={180/scale + 2} transform={`rotate(90 ${index*10+15}, ${180/scale + 5})`} key={index} info={this.props.info}>{date.replace(/_/g, " ") +" "}</text>)}
 				</svg>
 			)
 	}
--- birdwatching_app/src/Components/search_menu.js
+++ birdwatching_app/src/Components/search_menu.js
@@ -1,4 +1,5 @@
 import React from "react" 
+import searchMenuCSS from './search_menu.css'
 
 export default class SeachMenu extends React.Component {
 	state = {
@@ -7,12 +8,15 @@ export default class SeachMenu extends React.Component {
 	render() {
 		let sortedOptions = this.props.options.sort()
 			return(
-				<form onSubmit={event => {event.preventDefault(); this.props.makeGraph(this.state.selected)}}>
-					<select name="birds" onChange={event => this.setState({selected: event.target.value})}>
-						{sortedOptions.map((species, index) => <option value={species} key={index}>{species.replace(/_/g, " ")}</option>)}
-					</select>
-					<input type="submit"/>
-				</form>
+				<div className="search">
+					<form onSubmit={event => {event.preventDefault(); this.props.makeGraph(this.state.selected)}}>
+						<select name="birds" onChange={event => this.setState({selected: event.target.value})}>
+							{sortedOptions.map((species, index) => <option value={species} key={index}>{species.replace(/_/g, " ")}</option>)}
+						</select>
+						<input type="submit"/>
+					</form>
+					<br />
+				</div>
 			)
 	}
 }
